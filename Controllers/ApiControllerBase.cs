cd2a5d3dde47c1e76e8a635c22746fc2eddd288c
wizacass <visak.pet0@gmail.com>
2020-10-16 18:58:02 +0300

    Implement controller base and default endpoint to test the API

---
+++ Controllers/ApiControllerBase.cs
@@ -0,0 +1,65 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace API.Controllers
+{
+    public class ApiControllerBase : ControllerBase
+    {
+        private const string ApiHeader = "X-Api-Request";
+        protected const string ApiContentType = "application/json";
+
+        protected bool IsValidApiRequest()
+        {
+            Request.Headers.TryGetValue(ApiHeader, out var headers);
+            if (headers.Count != 1 || string.IsNullOrWhiteSpace(headers.FirstOrDefault()))
+            {
+                return false;
+            }
+
+            return headers == "true";
+        }
+
+        protected BadRequestObjectResult ApiBadRequest(string message)
+        {
+            var error = new ErrorDTO
+            {
+                Type = 400,
+                Title = message
+            };
+            return BadRequest(error);
+        }
+
+        protected NotFoundObjectResult ApiNotFound(string message)
+        {
+            var error = new ErrorDTO
+            {
+                Type = 404,
+                Title = message
+            };
+            return NotFound(error);
+        }
+
+        protected ObjectResult InternalServerError(string message, string details = null)
+        {
+            var error = new ErrorDTO
+            {
+                Type = 500,
+                Title = message,
+                Details = details
+            };
+            return StatusCode(500, error);
+        }
+
+        protected ObjectResult NotImplementedError(string message, string details = null)
+        {
+            var error = new ErrorDTO
+            {
+                Type = 501,
+                Title = message,
+                Details = details
+            };
+            return StatusCode(501, error);
+        }
+    }
+}
---
+++ Models/ErrorDTO.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace API.Models
+{
+    public class ErrorDTO
+    {
+        [JsonProperty("type")]
+        public long Type { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("details")]
+        public string Details { get; set; }
+    }
+}
