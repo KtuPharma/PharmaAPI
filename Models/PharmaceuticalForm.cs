0fd2c6312f956da8ff8b3a6d0e061ece77d33f7d
wizacass <visak.pet0@gmail.com>
2020-11-02 01:23:24 +0200

    Create Medicament model and seeder, establish db conncection

---
+++ Models/PharmaceuticalForm.cs
@@ -0,0 +1,20 @@
+﻿namespace API.Models.Enums
+{
+    public enum PharmaceuticalFormId : int
+    {
+        Tablets = 1,
+        Syrup,
+        Suspension,
+        Lozenge,
+        Spray,
+        Drops,
+        Ointment,
+        Injection
+    }
+
+    public class PharmaceuticalForm
+    {
+        public PharmaceuticalFormId Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Models/Seed/MedicamentSeed.cs
@@ -0,0 +1,56 @@
+﻿using API.Models.Enums;
+
+namespace API.Models.Seed
+{
+    public static class MedicamentSeed
+    {
+        public static void EnsureCreated(ApiContext context)
+        {
+            context.Medicaments.AddRange(
+                new Medicament
+                {
+                    Id = 1,
+                    Name = "TestDrug001",
+                    ActiveSubstance = "Dopamine",
+                    BarCode = "00001",
+                    RecipeRequired = false,
+                    IsReimbursed = false,
+                    Country = "Lithuania",
+                    Form = PharmaceuticalFormId.Spray
+                },
+                new Medicament
+                {
+                    Id = 2,
+                    Name = "TestDrug002",
+                    ActiveSubstance = "Methamphetamine",
+                    BarCode = "00002",
+                    RecipeRequired = true,
+                    IsReimbursed = false,
+                    Country = "Germany",
+                    Form = PharmaceuticalFormId.Tablets
+                },
+                new Medicament
+                {
+                    Id = 3,
+                    Name = "TestDrug003",
+                    ActiveSubstance = "Adderall",
+                    BarCode = "00003",
+                    RecipeRequired = true,
+                    IsReimbursed = true,
+                    Country = "USA",
+                    Form = PharmaceuticalFormId.Tablets
+                }, new Medicament
+                {
+                    Id = 4,
+                    Name = "TestDrug004",
+                    ActiveSubstance = "<Redacted>",
+                    BarCode = "00004",
+                    RecipeRequired = false,
+                    IsReimbursed = false,
+                    Country = "France",
+                    Form = PharmaceuticalFormId.Ointment
+                }
+            );
+        }
+    }
+}
