e08ec711dbee383a2e33a2e22d17a4f85a96d59f
k0vey <70900936+k0vey@users.noreply.github.com>
2020-12-07 16:51:09 +0200

    Add migration files

---
+++ Migrations/20201207143225_AddedWarehouseToOrdersAndOdersToWarehouse.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class AddedWarehouseToOrdersAndOdersToWarehouse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WarehouseId",
+                table: "Order",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_WarehouseId",
+                table: "Order",
+                column: "WarehouseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_Warehouse_WarehouseId",
+                table: "Order",
+                column: "WarehouseId",
+                principalTable: "Warehouse",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_Warehouse_WarehouseId",
+                table: "Order");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Order_WarehouseId",
+                table: "Order");
+
+            migrationBuilder.DropColumn(
+                name: "WarehouseId",
+                table: "Order");
+        }
+    }
+}
