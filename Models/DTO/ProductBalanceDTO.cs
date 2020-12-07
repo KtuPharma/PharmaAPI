1ed6707dc5ba20dbeab6bc09de79e1c68e8d3ef9
k0vey <70900936+k0vey@users.noreply.github.com>
2020-12-07 12:57:15 +0200

    Corrected ProductBalanceDTO to include the balance price and quantity

--- Models/DTO/ProductBalanceDTO.cs
+++ Models/DTO/ProductBalanceDTO.cs
@@ -9,11 +9,14 @@ namespace API.Models.DTO
     public class ProductBalanceDTO
     {
         [JsonProperty("name")] public string Name { get; }
+        [JsonProperty("price")] public decimal Price { get; }
+        [JsonProperty("quantity")] public int Quantity { get; }
 
         public ProductBalanceDTO(ProductBalance m)
         {
             Name = m.Medicament.Name;
-      
+            Price = m.Price;
+            Quantity = m.Quantity;
         }
     }
 }
